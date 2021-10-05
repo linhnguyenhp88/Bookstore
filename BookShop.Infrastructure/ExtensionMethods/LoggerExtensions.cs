ad9df866c2b34bcd4a8197589dda00629030009e
Linh Nguyen <linhnguyenhp88@gmail.com>
2021-10-05 15:48:08 +0700

    [2021-10-05] Add extension methods

---
+++ BookShop.Infrastructure/ExtensionMethods/LoggerExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.Infrastructure.ExtensionMethods
+{
+    /// <summary>
+    /// Add logs extension method 
+    /// </summary>
+    public static class LoggerExtensions
+    {
+        public static void LogCritical(this ILogger logger, Exception exception)
+        {
+            logger.LogCritical(default(EventId), exception, exception.GetInnerException().GenerateLogContent());
+        }
+
+        public static void LogError(this ILogger logger, Exception exception)
+        {
+            logger.LogError(default(EventId), exception, exception.GetInnerException().GenerateLogContent());
+        }
+    }
+}
