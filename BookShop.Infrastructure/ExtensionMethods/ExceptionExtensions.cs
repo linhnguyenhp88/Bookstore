27082a03fa0ae8a3764b78ca1d1adcca8a0d853b
Linh Nguyen <linhnguyenhp88@gmail.com>
2021-10-05 15:48:22 +0700

    [2021-10-05] Add extension methods

---
+++ BookShop.Infrastructure/ExtensionMethods/ExceptionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.Infrastructure.ExtensionMethods
+{
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Creates generate the content of log message
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static string GenerateLogContent(this Exception exception)
+        {
+            var content = exception.Message + Environment.NewLine + exception.StackTrace;
+            return content;
+        }
+
+        /// <summary>
+        /// Get inner exceptions
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        public static Exception GetInnerException(this Exception ex)
+        {
+            var innerException = ex;
+            
+            while(innerException.InnerException != null)
+            {
+                innerException = innerException.InnerException;
+            }
+
+            return innerException;
+        }
+    }
+}
